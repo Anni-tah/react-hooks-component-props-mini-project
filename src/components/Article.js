72474e92f18fa1fcdc20da654ea5104427e760c8
Anne Nabwire <nabanne96@gmail.com>
2025-02-14 19:59:46 +0300

    lab completed

---
+++ src/components/Article.js
@@ -0,0 +1,24 @@
+import React from "react";
+function Article({title,preview,date='January 1, 1970', time}){
+    function generateReadingEmoji(time){
+       
+        if (time<30){
+            const duration=Math.ceil(time/5);
+            return "☕️".repeat(duration)
+        
+        }else{
+            const duration =Math.ceil(time/10);
+            return "🍱".repeat(duration);
+
+        }
+       
+
+    }
+    return <article>
+        <h3>{title}</h3>
+        <small>{`${date}  ${generateReadingEmoji(time)}  ${time} Minutes`}</small>
+        <p>{preview}</p>
+    </article>
+   
+}
+export default Article;
